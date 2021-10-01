0938ffcf99527fb460106a3a67c2e49a914bfdad
Mykola Vlasov <mykolav@wix.com>
2021-10-01 23:14:12 +0300

    exercise 3 - main

--- src/exercise/03.tsx
+++ src/exercise/03.tsx
@@ -2,25 +2,35 @@
 // http://localhost:3000/isolated/exercise/03.tsx
 
 import * as React from 'react'
+import {useContext, useState} from 'react'
 
-// 🐨 create your CountContext here with React.createContext
+type CountContextT =
+  | [number, React.Dispatch<React.SetStateAction<number>>]
+  | undefined
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+const CountContext = React.createContext<CountContextT>(undefined)
+
+const CountProvider: React.FC = ({children}) => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <CountContext.Provider value={[count, setCount]}>
+      {children}
+    </CountContext.Provider>
+  )
+}
+
+const useCountContext = () => useContext(CountContext)
 
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCountContext()
+
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 replace the fake implementation of setCount with what you get back from useContext with the CountContext
-  const setCount = () => {}
-  // @ts-expect-error 💣
+  const [, setCount] = useCountContext()
+
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -28,12 +38,10 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
